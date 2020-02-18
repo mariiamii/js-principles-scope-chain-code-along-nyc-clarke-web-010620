d367a4b60d59fe05801dcd2c4deb87408524a3c5
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 04:59:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function upperCaseCustomerName(){
   customerName = customerName.toUpperCase();
 }
 
-// function setBestCustomer(){
-//   bestCustomer = 'not bob';
-// }
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
 
 // function overWriteBestCustomer(){
 //   bestCustomer = 'maybe bob';
