888dbb6e5b9b198d111dfe86e920f7c91623139a
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 04:41:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /* Your code goes here */
-// var customerName = 'bob';
-// const leastFavoriteCustomer = 'some value';
+var customerName = 'bob';
+const leastFavoriteCustomer = 'some value';
 
-// function upperCaseCustomerName() {
-//   customerName = customerName.toUpperCase();
-// }
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
 
-// function setBestCustomer() {
-//   bestCustomer = 'not bob';
-// }
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
 
-// function overwriteBestCustomer() {
-//   bestCustomer = 'maybe bob';
-// }
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
 
-// function changeLeastFavoriteCustomer() {
-//   leastFavoriteCustomer = 'billy';
-// }
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'billy';
+}
