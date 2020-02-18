4723e6567f611effd891b9deff4cc2bf19058043
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 04:58:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,55 +2,26 @@
 // const leastFavoriteCustomer = 'some value';
 
 // function upperCaseCustomerName(){
-//   customerName = customerName.toUpperCase();
-// }
+  customerName = customerName.toUpperCase();
+}
 
-// function setBestCustomer(){
-//   bestCustomer = 'not bob';
-// }
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
 
-// function overWriteBestCustomer(){
-//   bestCustomer = 'maybe bob';
-// }
-
-// function changeLeastFavoriteCustomer(){
-//   leastFavoriteCustomer = 'billy';
-// }
-
-// function reassignLeastFavorite(){
-//   leastFavoriteCustomer = 'bobby';
-// }
-
-// function attemptTwoFavoriteCustomers(){
-//     let favoriteCustomer = 'bob';
-//     let favoriteCustomer = 'billy';
-// }
-
-var customerName = 'bob'
-const leastFavoriteCustomer = 'sour fred'
-
-function upperCaseCustomerName() {
-  return customerName = customerName.toUpperCase()
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
 }
 
 function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'sour fred'
+  leastFavoriteCustomer = 'billy';
 }
 
-
-function setBestCustomer(){
-  bestCustomer = 'not bob'
-}
-
-function overWriteBestCustomer(newFavorite){
-  bestCustomer = newFavorite
-}
-
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
+function reassignLeastFavorite(){
+  leastFavoriteCustomer = 'bobby';
 }
 
 function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
+    let favoriteCustomer = 'bob';
+    let favoriteCustomer = 'billy';
 }
